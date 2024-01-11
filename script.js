b855431b6c5e37d186291b4f164367d223e2f40b
kailashgautham <kailash.gautham@gmail.com>
2024-01-11 23:14:17 +0800

    added list add and removal with delete

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
 var button = document.getElementsByTagName("button")[0];
+var input = document.getElementById("input");
+var list = document.getElementsByTagName("ul")[0];
+var listItems = document.getElementsByTagName("li");
 
-button.addEventListener("click", function() {
-    var input = document.getElementById("input").value;
-    var list = document.getElementsByTagName("ul")[0];
+var addToListAfterClick = function() {
+    if (input.value === "") return;
+    addToList();
+};
+
+var addToListAfterEnter = function(event) {
+    if (event.key !== "Enter" || input.value === "") return;
+    addToList();
+};
+
+var addToList = function() {
+    var text = input.value;
+    if (text === "") return;
     var li = document.createElement("li");
-    li.appendChild(document.createTextNode(input));
+    var div = document.createElement("div");
+    div.classList.add("button");
+    var button = document.createElement("button");
+    button.appendChild(document.createTextNode("Delete"));    
+    div.appendChild(button);
+    li.appendChild(document.createTextNode(text));
+    li.appendChild(div);
     list.appendChild(li);
-});
+    li.addEventListener("click", toggleDone);
+    li.lastChild.lastChild.addEventListener("click", deleteItem);
+    input.value = "";
+};
+
+var deleteItem = function() {
+    this.parentElement.parentElement.remove();
+}
+var toggleDone = function() {
+    this.classList.toggle("done");
+}
+
+button.addEventListener("click", addToListAfterClick);
+
+input.addEventListener("keyup", addToListAfterEnter);
+
+for (let listItem of listItems) {
+    listItem.addEventListener("click", toggleDone);
+    listItem.lastChild.children[0].addEventListener("click", deleteItem);
+}
