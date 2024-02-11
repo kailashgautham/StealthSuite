2ca8960a60e3257a423b9c9bae739bdfa7d6d667
kailashgautham <kailash.gautham@gmail.com>
2024-02-11 15:22:40 +0800

    Start project with create-react-app and working on frontend

--- README.md
+++
@@ -1,7 +0,0 @@
-# VisionScope
-VisionScope is a full-stack application that uses machine learning to power face detection.
-
-# Tech Stack
-Frontend: ReactJS
-
-Backend: NodeJS, ExpressJS, PostgreSQL
---
+++ frontend/src/components/Logo/Logo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Tilt from 'react-parallax-tilt';
+import './Logo.css';
+import brain from './brain.png';
+
+const Logo = () => {
+    return (
+        <div className='ma4 mt0'>
+            <Tilt className="Tilt br2 shadow-2" options={{max: 55}} style={{ height: '150px', width: '150px', backgroundColor: '' }}>
+                <div className="Tilt-inner pa3">
+                    <img src={brain} alt="logo"></img>
+                </div>
+            </Tilt>
+        </div>
+    )
+};
+
+export default Logo;
