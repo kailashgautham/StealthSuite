a393ebff97759335483a6eb2ee447f54e51659a0
kailashgautham <kailash.gautham@gmail.com>
2024-03-04 16:19:28 +0800

    Add express.js server files

--- node/server.js
+++ node/server.js
@@ -1,8 +1,34 @@
 import express from 'express';
+import fs from 'fs';
+// const app = express();
 
-const app = express();
+// //middleware to do something with the response before sending it to the actual call
+// // app.use(express.urlencoded({extended: false}));
+// // app.use(express.json());
+// app.use(express.static(process.cwd() + '/public'));
 
-app.get("/", (req, res) => {
-    res.send("hello");
+// app.listen(3000);
+fs.readFile('./script.js', (err, data) => {
+    if (err) {
+        console.log("error lmao")
+    }
+    else {
+        console.log("1: ", data.toString('utf8'));
+    }
 })
-app.listen(3000);
+
+const file = fs.readFileSync('./script.js');
+console.log("2: ", file.toString());
+
+fs.appendFile('./script.js', 'I actually appended this stuff now.', err => {
+    if (err) console.log(err);
+});
+
+fs.writeFile('./bye.txt', 'sad to see you go', err => {
+    if (err) console.log(err);
+});
+
+fs.unlink('./bye.txt', err => {
+    if (err) console.log(err);
+})
+
